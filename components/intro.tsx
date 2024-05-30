c9a0307457594f31d5fa44846ecca9896e22bc6c
Himanshu Saini <55054295+Himanshu-dev-web@users.noreply.github.com>
2024-05-30 22:22:35 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Himanshu.</span> I'm a{" "}
         <span className="font-bold">Frontend developer</span> with{" "}
-        <span className="font-bold">6 Months</span> of experience. I enjoy
+        <span className="font-bold">8 Months</span> of experience. I enjoy
         building <span className="italic">sites & Web Apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
